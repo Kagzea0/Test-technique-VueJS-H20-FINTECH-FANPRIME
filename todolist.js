2ba83d5bc232a1dd8975665396489957448b9714
Kagzea <erwan.michaudwinkler@gmail.com>
2024-08-21 15:58:19 +0200

    com2
    Ajout de nouvelles méthodes dans todolist.js

--- todolist.js
+++ todolist.js
@@ -2,23 +2,24 @@ const main = {
     template: `
         <div>
             <h1>Todo List</h1>
-
             <input v-model="newTask" @keyup.enter="addTask" placeholder="New task">
 
-            <!-- Bouton pour supprimer toutes les tâches -->
-            <button @click="ClearTasks">Delete tasks</button>
+            <button @click="deleteAllTasks">Delete tasks</button>
 
-            <!-- Affichage du nombre de tâches complétées -->
-            <span>  </span>
-            <span> Tasks done </span>
-            
-            <!-- Affichage du nombre de tâches en tout -->
-            <span>  </span>
-            <span> Tasks </span>
+            <span> {{ completedTasks }} </span> <span> Tasks done </span>
+
+            <span> {{ tasks.length }} </span> <span> Tasks </span>
+        
 
             <ul>
-                <li>
-                
+                <li v-for="(task, index) of tasks" :key="index">
+                    <input type="checkbox" :class="'taskCheckbox'" v-model="task.completed" @change="updateTask">
+                    <span v-if="!editingTask || editingTaskIndex !== index" :class="'taskName'">{{ task.name }}</span>
+
+                    <input :class="'editTask'" v-else v-model="editedTask" @keyup.enter="saveTask(index)">
+
+                    <button @click="editTask(index)">Edit</button>
+                    <button @click="deleteTask(index)">Delete</button>
                 </li>
             </ul>
         </div>
@@ -26,19 +27,77 @@ const main = {
     data() {
         return {
             newTask: '',
-            tasks: []
+            tasks: JSON.parse(localStorage.getItem('tasks')) || [],
+            editingTask: false,
+            editingTaskIndex: null,
+            editedTask: '',
+            completedTasks: 0
         };
     },
     methods: {
         // Méthode addTask
+        addTask() {
+            if (this.newTask.trim()) {
+                this.tasks.push({ name: this.newTask.trim(), completed: false });
+                this.newTask = '';
+                this.saveTask();
+            }
+        },
+
         // Méthode editTask
+        editTask(index) {
+            if (this.editingTask && this.editingTaskIndex === index) {
+                this.saveTask(index);
+            }
+            else {
+                this.editingTask = true;
+                this.editingTaskIndex = index;
+                this.editedTask = this.tasks[index].name;    
+            }
+        },
+
         // Méthode saveTask
+        saveTask(index) {
+            if (this.editedTask.trim()) {
+                this.tasks[index].name = this.editedTask.trim();
+                this.editingTask = false;
+                this.editingTaskIndex = null;
+                this.editedTask = '';
+                this.saveAllTasks();
+            }
+        },
+
         // Méthode saveAllTasks
+        saveAllTasks() {
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+        },
+
         // Méthode deleteTask
+        deleteTask(index) {
+            if (this.tasks[index].completed) {
+                this.completedTasks--;
+            }
+            this.tasks.splice(index, 1);
+            this.saveAllTasks();
+        },
+
         // Méthode deleteAllTasks
+        deleteAllTasks() {
+            this.tasks = [];
+            this.completedTasks = 0;
+            this.saveAllTasks();
+        },
 
         // Méthode updateTask (si j'ai le temps)
+        updateTask() {
+            this.completedTasks = this.tasks.filter(task => task.completed).length;
+        }        
+    },
+    created() {
+        this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        this.completedTasks = this.tasks.filter(task => task.completed).length;
     }
+
 };
 
 export default main;
